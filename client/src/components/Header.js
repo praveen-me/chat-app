fd90b384296b1498d9c8da5397c7347796888426
praveen-me <praveenkrsaini0@gmail.com>
2019-01-02 10:50:55 +0530

    Add signLink and signout links and also style them.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 class Header extends Component {
   render() {
+    const {user} = this.props;
+    
     return (
       <header>  
         <div className="wrapper">
           <h1 className="logo">Chit Chatter</h1>
-          <div className="auth-buttons">
-    
-          </div>
+          {
+            !user._id ? (
+              <div className="auth-buttons">
+                <Link to="/signup" className="auth-buttons">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="auth-buttons">
+                  Log In
+                </Link>
+              </div>
+            ) : (
+              <div className="auth-buttons">
+                <a className="auth-buttons">
+                  Log out
+                </a>
+              </div>
+            )
+          }
         </div>
       </header> 
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    user : state.user
+  }
+}
+
+export default connect(mapStateToProps)(Header);
